ee5c4db1e8dbeb79a759e323624af95feb69f228
chandra <chandrasekharkasa@gmail.com>
2024-07-25 23:01:59 +0530

    second test case

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,4 +2,4 @@
 echo "Ramesh:: Hello suresh, How are you ?"
 echo "Suresh:: Hi Ramesh, I am fine. How are you ?"
 echo "Ramesh:: I am fine too. how is your work?"
-echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
+echo "Suresh:: not bad. I am thinking to upgrade to DevOpss"
