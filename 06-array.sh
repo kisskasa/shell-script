c181b6b81c20f9b838a9201aa1f9a805d08211ef
chandra <chandrasekharkasa@gmail.com>
2024-07-31 13:41:59 +0530

    commit 1

--- 06-array.sh
+++ 06-array.sh
@@ -5,11 +5,3 @@ MOVIES=("RRR" "DjTillu" "murari")
 echo " First Movie is: ${MOVIES[0]}"
 echo " First Movie is: ${MOVIES[1]}"
 echo " First Movie is: ${MOVIES[@]}"
-
-echo " First Movie is: $MOVIES[0]"
-echo " First Movie is: $MOVIES[1]"
-echo " First Movie is: $MOVIES[@]"
-
-echo " First Movie is: {MOVIES[0]}"
-echo " First Movie is: {MOVIES[1]}"
-echo " First Movie is: {MOVIES[@]}"
