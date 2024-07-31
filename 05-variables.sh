ea5f66e7959aeb409906ef95014953a666807356
chandra <chandrasekharkasa@gmail.com>
2024-07-31 13:27:42 +0530

    commit 1

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Please enter username:"
-read USERNAME
+read -s USERNAME
 
 echo "Please enter password:"
-read PASSWORD 
+read -s PASSWORD 
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
 
