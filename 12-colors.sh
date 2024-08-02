394e5bbb0964bdff6a8d6f9e01feb50470a54cf4
chandra <chandrasekharkasa@gmail.com>
2024-08-02 21:22:55 +0530

    message 1

--- 12-colors.sh
+++ 12-colors.sh
@@ -33,5 +33,5 @@ VALIDATE $? "Installing MYSQL"
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing GIT"
 
-dnf install dockerr -y &>>$LOGFILE
+dnf install docker -y &>>$LOGFILE
 VALIDATE $? "Installing Docker"
