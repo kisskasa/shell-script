8c2e761e398b2616bd90edd5444aa6d3ed506d7e
chandra <chandrasekharkasa@gmail.com>
2024-08-04 12:33:37 +0530

    message 1

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE()
 {
@@ -42,7 +44,7 @@ VALIDATE $? "Start SQL Server"
 
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup ...$Y SKIPPING $N"
