3bc45b1a102552a9fe14026f43893a92d9d2e5ff
chandra <chandrasekharkasa@gmail.com>
2024-08-04 12:18:32 +0530

    message 1

--- mysql.sh
+++ mysql.sh
@@ -37,8 +37,17 @@ VALIDATE $? "Enabling SQL Server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Start SQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup ...$Y SKIPPING $N"
+fi
+    
 
 
 
