f9b50c2af1abf02dc6f8edb5d503e010ebcfe8cf
chandra <chandrasekharkasa@gmail.com>
2024-08-04 12:58:37 +0530

    message 1

--- backend.sh
+++ backend.sh
@@ -42,7 +42,7 @@ then
     useradd expense &>>LOGFILE
     VALIDATE $? "Creating expense user"
 else
-    echo "Expense user already created ... $Y SKIPPING $N "
+    echo -e "Expense user already created ... $Y SKIPPING $N "
 fi
 
 
