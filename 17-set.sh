c70281665f022f7854017c18c3dfdf02663bf738
chandra <chandrasekharkasa@gmail.com>
2024-08-05 19:52:52 +0530

    message 1

---
+++ 17-set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "you are super user."
+fi
+
+dnf install mysql11 -y
+dnf install git -y
+
+echo "is script proceeding."
