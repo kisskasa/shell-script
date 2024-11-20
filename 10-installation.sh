059d650e03e6f4368465463139803ed1675d1ccc
kisskasa <chandrasekharkasa@gmail.com>
2024-11-20 20:25:36 +0530

    sg

--- 10-installation.sh
+++ 10-installation.sh
@@ -16,8 +16,18 @@ if [ $? -ne 0 ]
 then
     echo "Installation of mysql ...FAILURE"
     exit 1
+else
+    echo "Installation of mysql ...SUCCESS"
 fi
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT ...FAILURE"
+    exit 1
+else
+    echo "Installation of GIT ...SUCCESS"
+fi
 
 echo "Is script proceeding?"
