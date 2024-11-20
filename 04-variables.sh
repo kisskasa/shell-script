556b79fa8b77be34b30bafc07255f7f833c1b809
kisskasa <chandrasekharkasa@gmail.com>
2024-11-20 13:58:09 +0530

    sg

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, how are you ?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you ?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2::: not bad, I am thinking to upgrade to DevOps"
