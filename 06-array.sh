6340a9a2ea848ce4fc7063d67660ffe298282e7e
kisskasa <chandrasekharkasa@gmail.com>
2024-11-20 17:07:42 +0530

    sg

---
+++ 06-array.sh
@@ -0,0 +1,7 @@
+MOVIES=("RRR" "DJTILLU", "Murrali")
+
+echo "First Movie is: $MOVIES[0]"
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[2]}"
