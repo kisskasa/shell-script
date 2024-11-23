45d9cc4d2d096b84978bcc92c0a974618e7c8715
kisskasa <chandrasekharkasa@gmail.com>
2024-11-23 13:33:08 +0530

    sg

---
+++ 17-set.sh
@@ -0,0 +1,16 @@
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "You are super user."
+fi
+
+dnf install mysql123 -y
+dnf install git -y
+
+echo "Is script proceeding?"
